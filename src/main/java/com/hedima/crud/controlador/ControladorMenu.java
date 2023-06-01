334f47f43f43e26e4f0ea37afdb24d43274fa9a9
TitaRuiz <ggp2002es@gmail.com>
2023-06-01 21:09:54 +0200

    final de clase hasta eliminar

--- src/main/java/com/hedima/crud/controlador/ControladorMenu.java
+++ src/main/java/com/hedima/crud/controlador/ControladorMenu.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,5 +24,24 @@ public class ControladorMenu {
         modelo.addAttribute("menus",menus);
         return "mostrar";
 
+    }
+    @GetMapping("/formulario")
+    public String mostrarFormulario(Model modelo) {
+        Menu menu = new Menu();
+        modelo.addAttribute("menu",menu);
+        return "formulario";
+    }
+
+    @PostMapping("/alta")
+    public String guardarMenu(Menu m, RedirectAttributes ra) {
+        Menu m1 = servicio.crear(m);
+        System.out.println(m1.toString());
+        return "redirect:/menus";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarMenu(@PathVariable("id") Integer id){
+        servicio.eliminar(id);
+        return "redirect:/menus";
     }
 }
